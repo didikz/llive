894150481a09853d4b126425322d18c0ca482861
didikz <rezd14@gmail.com>
2024-11-01 21:41:36 +0700

    fix docker service backend

--- project/backend/src/index.php
+++ project/backend/src/index.php
@@ -1,29 +1,35 @@
 <?php
+ob_start();
 require 'Controller/UserController.php';
 require 'Service/UserService.php';
 require 'Model/User.php';
 
 // Example usage
-$db = new mysqli('127.0.0.1', 'root', '', 'little_lives');
+$db = new mysqli('host.docker.internal', 'root', '', 'little_lives');
 $userService = new UserService($db);
 $userController = new UserController($db);
 
 try {
     $request = $_SERVER['REQUEST_URI'];
-    // CORS headers
-    header("Access-Control-Allow-Origin: *");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
     $uris = explode("/", $request);
 
     switch ($uris[2]) {
         case 'user':
             $user = $userController->getUser($uris[3]);
+            // CORS headers
+            header("Access-Control-Allow-Origin: *");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
             header('Content-Type: application/json; charset=utf-8');
             echo json_encode($user);
             break;
         default:
-            header('HTTP/1.1 404 Not Found');
+            // CORS headers
+            header("Access-Control-Allow-Origin: *");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode(['error' => "not found"]);
             break;
     }
 } catch (Exception $e) {
