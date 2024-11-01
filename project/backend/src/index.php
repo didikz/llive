8ddd03e0e0e00aa11e322a8cb79b0ff1b9ea4f96
didikz <rezd14@gmail.com>
2024-11-01 20:57:17 +0700

    adjust API routing

--- project/backend/src/index.php
+++ project/backend/src/index.php
@@ -4,13 +4,28 @@ require 'Service/UserService.php';
 require 'Model/User.php';
 
 // Example usage
-$db = new mysqli('localhost', 'root', '', 'little_lives');
+$db = new mysqli('127.0.0.1', 'root', '', 'little_lives');
 $userService = new UserService($db);
 $userController = new UserController($db);
 
 try {
-    $user = $userController->getUser(1);
-    print_r($user);
+    $request = $_SERVER['REQUEST_URI'];
+    // CORS headers
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    $uris = explode("/", $request);
+
+    switch ($uris[2]) {
+        case 'user':
+            $user = $userController->getUser($uris[3]);
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode($user);
+            break;
+        default:
+            header('HTTP/1.1 404 Not Found');
+            break;
+    }
 } catch (Exception $e) {
     echo $e->getMessage();
 }
